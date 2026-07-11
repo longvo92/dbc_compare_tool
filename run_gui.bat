d779298d110d75932d88da1e31639aa11c20d404
longvo920 <vtlong29@gmail.com>
2026-07-12 02:08:32 +0700

    fix bat

---
+++ run_gui.bat
@@ -0,0 +1,8 @@
+@echo off
+setlocal
+cd /d "%~dp0"
+if not exist ".venv\Scripts\python.exe" (
+    echo venv missing. run: python -m venv .venv
+    exit /b 1
+)
+.venv\Scripts\python.exe -m dbc_compare_tool %*
